bb6d38844512f5af910d7e0642b9ab337e422e88
waldirmerjildo <muwaldir@gmail.com>
2024-03-03 01:26:06 -0500

    creacion de las rutas y los controlleres de actaulizacion, delete

--- api/src/controllers/project.controller.js
+++ api/src/controllers/project.controller.js
@@ -12,3 +12,45 @@ export const createProject = async (req, res) => {
   const saveProject = await newProject.save();
   res.json(saveProject);
 };
+
+export const getProjectsId = async (req, res, next) => {
+    const projectId = req.params.id
+    const project = await Project.findById(projectId)
+    res.json(project);
+};
+
+export const deleteProject = async (req, res, next) => {
+    const projectId = req.params.id
+    try {
+        // Buscar el proyecto por su ID y eliminarlo
+        const deletedProject = await Project.findByIdAndDelete(projectId);
+        if (!deletedProject) {
+          return res.status(404).json({ error: 'Proyecto no encontrado' });
+        }
+        res.status(200).json({ message: 'Proyecto eliminado exitosamente' });
+      } catch (error) {
+        console.error('Error al eliminar el proyecto:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      }
+}
+
+export const updateProjectById = async (req, res) => {
+    const { id } = req.params; // Obtener el ID del proyecto de los parámetros de la solicitud
+    const { title, description, images, link, tech, type,destacado } = req.body; // Obtener los datos actualizados del cuerpo de la solicitud
+  
+    try {
+      const updatedProject = await Project.findByIdAndUpdate(
+        id, // ID del proyecto que se va a actualizar
+        { title, description, images, link, tech, type, destacado }, // Datos actualizados del proyecto
+        { new: true } // Opción { new: true } para devolver el documento actualizado
+      );
+  
+      if (!updatedProject) {
+        return res.status(404).json({ message: "Proyecto no encontrado" });
+      }
+  
+      res.json(updatedProject); // Devolver el proyecto actualizado
+    } catch (error) {
+      res.status(500).json({ message: "Error al actualizar el proyecto", error: error.message });
+    }
+  };
--- api/src/routes/projects.routes.js
+++ api/src/routes/projects.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createProject, getProjects } from "../controllers/project.controller.js";
+import { createProject, deleteProject, getProjects, getProjectsId, updateProjectById } from "../controllers/project.controller.js";
 
 const router =  Router();
 router.get('/projects', getProjects)
+router.get('/projects/:id', getProjectsId)
 router.post('/projects', createProject)
+router.post('/projects', createProject)
+
+router.put('/projects/:id', updateProjectById)
 
 export default router;
